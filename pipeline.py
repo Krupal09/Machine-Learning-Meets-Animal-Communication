7dd9abff8a8b26b883d207e57896e612cdd882af
Krupal09 <49275269+Krupal09@users.noreply.github.com>
2021-04-19 15:57:06 +0200

    plot clustered mean points in different colours

--- pipeline.py
+++ pipeline.py
@@ -267,7 +267,8 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 r = [-1,1]
 X, Y = np.meshgrid(r, r)
-ax.scatter3D(points[:, 0], points[:, 1], points[:, 2])
+# plot clustered mean points in 7 different colours
+ax.scatter3D(points[:, 0], points[:, 1], points[:, 2], color=["red","black","blue","orange","yellow","green","grey"])
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
